c3aee98d297c33cf88aeecf3132477c98ad43ffa
adilhannouni <adilhannouni@gmail.com>
2020-10-09 00:17:47 -0700

    Add state toggle code

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import Transition from 'react-transition-group/Transition';
 
 class TransitionComp extends Component{
 
@@ -7,9 +8,16 @@ class TransitionComp extends Component{
         show:false
     }
 
+    showToggle = () => {
+        this.setState({
+            show:!this.state.show ? true : false
+        })
+    }
+
     render(){
         return(
-        <div>{ this.state.show ? <div style={{
+        <div>
+        {/* { this.state.show ? <div style={{
                     background: 'lightgray',
                     height:'100px'
                 }}>
@@ -17,7 +25,15 @@ class TransitionComp extends Component{
             </div>
             :
             null
-        }
+        } */}
+       <Transition in={this.state.show}
+       timeout= {2000}
+       >
+        {state => <p>{state}</p>}
+       </Transition>
+        <div className='showDiv' onClick={this.showToggle}>
+            Show or Hide Component
+        </div>
         </div>
         )
     }
